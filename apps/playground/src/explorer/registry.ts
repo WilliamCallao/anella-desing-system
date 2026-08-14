d25869769dd97a4114e65cf635275f914b1541af
unknown <callaowilliamhumberto@gmail.com>
2026-08-14 12:34:09 -0400

    feat(playground): register Inputs category in explorer

--- apps/playground/src/explorer/registry.ts
+++ apps/playground/src/explorer/registry.ts
@@ -4,13 +4,14 @@ import { checklists } from "./categories/checklists";
 import { dialogos } from "./categories/dialogos";
 import { filtros } from "./categories/filtros";
 import { formularios } from "./categories/formularios";
+import { inputs } from "./categories/inputs";
 import { other } from "./categories/other";
 import { tipografia } from "./categories/tipografia";
 import type { ComponentCategory } from "./types";
 
 export type { ComponentCategory, ComponentEntry, VariantDemo } from "./types";
 
-export const componentCategories = [formularios, animaciones, botones, dialogos, checklists, filtros, other, tipografia];
+export const componentCategories = [formularios, inputs, animaciones, botones, dialogos, checklists, filtros, other, tipografia];
 
 export function findCategory(id: string): ComponentCategory | undefined {
   return componentCategories.find((category) => category.id === id);
